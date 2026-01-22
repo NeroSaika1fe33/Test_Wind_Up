adf0b14063f974c9b57ec49f930e9fb017f63922
NeroSaika1fe33 <188322718+NeroSaika1fe33@users.noreply.github.com>
2026-01-23 00:17:14 +0900

    CreateItemContact

--- Assets/Script/Track/Track.cs
+++ Assets/Script/Track/Track.cs
@@ -6,12 +6,16 @@ public class Track : MonoBehaviour
     public CameraTrack[] introTracks;
     public Checkpoint[] checkpoints;
     public Transform[] startLinePos;
+    public Transform[] ItemPos;
     public int TrackID = 0;
+    public GameObject[] Item;
+
 
     private void Awake()
     {
         GameManager.Instance.SetCurrentTrack(this);
         InitCheckpoints();
+        InitItemPos();
     }
 
     private void InitCheckpoints()
@@ -19,6 +23,14 @@ public class Track : MonoBehaviour
         for(int i = 0; i < checkpoints.Length; i++)
         {
             checkpoints[i].index = i;
+        }
+    }
+
+    private void InitItemPos()
+    {
+        for(int i = 0;i<ItemPos.Length;i++)
+        {
+            var item=Instantiate(Item[i],ItemPos[i]);
         }
     }
 }
